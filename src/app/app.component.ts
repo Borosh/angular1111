9eb969b014a17a56d7f872eb379481541dcabfd7
Adam Boros <aboros97@gmail.com>
2021-11-11 11:29:02 +0100

    Table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  persons = [
+    {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      gender: 'male',
+    },
+    {
+      name: 'Luke Skywalker1',
+      height: '172',
+      mass: '77',
+      gender: 'male',
+    },
+    {
+      name: 'Luke Skywalker2',
+      height: '172',
+      mass: '77',
+      gender: 'male',
+    },
+  ];
+}
