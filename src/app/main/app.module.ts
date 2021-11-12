dbcc6bcc609be3b61925bd8c8bf887c4ccceb017
Borosh <aboros97@gmail.com>
2021-11-12 11:43:43 +0100

    Add material

--- src/app/main/app.module.ts
+++ src/app/main/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './containers/main/main.component';
 import { SharedModule } from '../shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent, MainComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, SharedModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
