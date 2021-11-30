ce3def3e5c5d9342d950ad2b05225ae468b18ab8
Borosh <aboros97@gmail.com>
2021-11-30 22:42:21 +0100

    Load only component

--- shell/src/app/main/app.component.ts
+++ shell/src/app/main/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import {
+  Component,
+  ComponentFactoryResolver,
+  Injector,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +14,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('vc', { read: ViewContainerRef, static: true })
+  viewContainer!: ViewContainerRef;
+
   isSidenavOpened: boolean;
 
+  constructor(
+    private router: Router,
+    private injector: Injector,
+    private resolver: ComponentFactoryResolver
+  ) {
     window.addEventListener('fromPersonList', () => {
       console.log('GOTCHA');
     });
+  }
 
   navigateTo(path: string) {
     this.router.navigateByUrl(path);
@@ -19,5 +35,21 @@ export class AppComponent {
 
   onMenuClick() {
     this.isSidenavOpened = !this.isSidenavOpened;
+    if (!this.isSidenavOpened) {
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:4300/remoteEntry.js',
+        remoteName: 'person',
+        exposedModule: './ButtonComponent',
+      })
+        .then((m) => {
+          return m.ButtonComponent;
+        })
+        .then((comp) => {
+          const factory = this.resolver.resolveComponentFactory(comp);
+          this.viewContainer.createComponent(factory, undefined, this.injector);
+        });
+    }
   }
+
+  ngAfterViewInit() {}
 }
